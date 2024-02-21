c5962acb91b7237e4b813a91f1ef0423459df770
nati2331 <natnael.mk2@gmail.com>
2024-02-21 02:26:04 -0800

    five2

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-""" answer for question 5"""
+"""
+Technical interview preparation
+"""
 
 def island_perimeter(grid):
-    """ hat returns the perimeter of the island described in grid """
-     a = 0
-     for x in range(0, len(grid), 1):
-         for b in range(0, len(grid[0], 1):
-                 if grid[x][b] == 1:
-                    a = a + 4
-                    if b - 1 >= 0 and grid[x][b -1] == 1
-                        a -= 2
-                    if x -1 >= 0 grid[x - 1][b] == 1
-                        a -= 2
-    return (a)
+    """ Returns the perimeter of island described"""
+    perimeter = 0
+
+    num_rows = len(grid)
+
+    if grid != []:
+        num_columns = len(grid[0])
+
+    for row in range(num_rows):
+        for col in range(num_columns):
+            if grid[row][col] == 1:
+                if (row - 1) == -1 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if (row + 1) == num_rows or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if (col - 1) == -1 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if (col + 1) == num_columns or grid[row][col + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
 
